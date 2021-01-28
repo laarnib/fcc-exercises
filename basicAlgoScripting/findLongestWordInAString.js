7cd4ad688f231bfe0435bac84e450ae2d6dad024
Laarni Bernardo <laarnijb@gmail.com>
2021-01-28 12:23:08 -0500

    Removed try/catch and replaced with checking if a string is passed to the function.

--- basicAlgoScripting/findLongestWordInAString.js
+++ basicAlgoScripting/findLongestWordInAString.js
@@ -2,13 +2,17 @@
 //Your response should be a number.
 
 function findLongestWordLength(str) {
-    try {
+    //Check if string passed in parameter is empty
+    if (str === null) {
+        return "You forgot to specify a string.";
+    }
+    else {
         // Break the string into substrings
         let subStrArr = str.split(' ');
 
         // Save character count of first element in mostCharCount
-        let mostCharCount = subStrArr[0].length;
-  
+        et mostCharCount = subStrArr[0].length;
+
         // Traverse through the array comparing the length of each element
         // to the value in mostCharCount
         for (let word of subStrArr) {
@@ -16,21 +20,18 @@ function findLongestWordLength(str) {
                 mostCharCount = word.length;
             }
         }
-  
+
         return mostCharCount;
-    } catch(e) {
-        console.log("You forgot to include a string.")
-        console.log(e);
     }
-  }
+}
   
-  //Testing
-  findLongestWordLength("The quick brown fox jumped over the lazy dog");  //6
-  findLongestWordLength("May the force be with you"); //5
-  findLongestWordLength("Google do a barrel roll"); //6
-  findLongestWordLength("What is the average airspeed velocity of an unladen swallow"); //8
-  findLongestWordLength("What if we try a super-long word such as otorhinolaryngology"); //19
-  findLongestWordLength();
+//Testing
+findLongestWordLength("The quick brown fox jumped over the lazy dog");  //6
+findLongestWordLength("May the force be with you"); //5
+findLongestWordLength("Google do a barrel roll"); //6
+findLongestWordLength("What is the average airspeed velocity of an unladen swallow"); //8
+findLongestWordLength("What if we try a super-long word such as otorhinolaryngology"); //19
+findLongestWordLength();
 
 
 
