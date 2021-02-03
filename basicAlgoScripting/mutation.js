f90baaa82c7619239bcdce6402baed7a84dd2aca
Laarni Bernardo <laarnijb@gmail.com>
2021-02-03 10:56:08 -0500

    Improved the algorithm by using String.prototype.indexOf method.

--- basicAlgoScripting/mutation.js
+++ basicAlgoScripting/mutation.js
@@ -7,44 +7,19 @@
 // Lastly, ["Alien", "line"], should return true because all of the letters 
 // in "line" are present in "Alien".
 
-function removeDuplicateLetters(arr) {
-    // Split the strings into an array of characters split into 
-    let charArr = arr.split('');
-
-    //find duplicate strings and remove from the array
-    for (let i = 0; i < charArr.length; i++) {
-        for (let j = i + 1; j < charArr.length; j++) {
-            if (charArr[i] === charArr[j]) {
-                charArr.splice(j,1);
-                j -= 1;
-            }
-        }
-    }
-    return charArr;
-}
-
 function mutation(arr) {
 
-    // Remove duplicate letters in the strings;
-    let word1 = removeDuplicateLetters(arr[0]);
-    let word2 = removeDuplicateLetters(arr[1]);
-    let counter = 0;
+    // Transform the strings into lowercase
+    let word1 = arr[0].toLowerCase();
+    let word2 = arr[1].toLowerCase();
+    
     // Check if word 1 contains all letters of word2
     for (let i = 0; i < word2.length; i++) {
-        for (let j = 0; j < word1.length; j++) {
-            if(word1[j].toLowerCase() === word2[i].toLowerCase()) {
-                //Increment counter if character is the same
-                counter++;
-            }
+        if (word1.indexOf(word2[i]) < 0) {
+            return false;
         }
     }
-    
-    // Return true if counter is equal to length of the second element
-    if (counter === word2.length) {
-        return true;
-    } else {
-        return false;
-    }
+    return true;
 }
 
 mutation(["hello", "Hello"]);
